cb5717f2a4cae985e6e1f24df01fa5e5de7d3286
archarry86 <archarry86@gmail.com>
2016-12-18 00:51:56 -0500

    Animator using blendtress , Refactoring on PlayerController and new Sprites

--- 2DGame/Assets/Scripts/PlayerController.cs
+++ 2DGame/Assets/Scripts/PlayerController.cs
@@ -22,7 +22,8 @@ public class PlayerController : MonoBehaviour {
 
 	private bool facingRight;
 
-
+	//TODO HOW TO ENABLED AND DISABLED SOME COLIDERS
+	//private BoxCollider2D onliyingcolider;
 
 	// Use this for initialization
 	void Start () {
@@ -31,42 +32,58 @@ public class PlayerController : MonoBehaviour {
 		animator= this.GetComponent<Animator> ();
 		rigidBody2D= this.GetComponent<Rigidbody2D> ();
 
+	
+		/*var coliders = this.GetComponents<BoxCollider2D> (); 
+		onliyingcolider = coliders[coliders.Length-1];
+		onliyingcolider.enabled = false;
+*/
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		
 		if(onground){
-
-			if (Input.GetAxis ("Jump") > 0  ) {
-				onground = false;
-				animator.SetBool ("onGround", onground);
-				//adds a force to jump
+			
+			if (Input.GetAxis ("Jump") > 0.1  ) {
+				animator.SetBool ("isjumping",true);
 				rigidBody2D.AddForce (new Vector2 (0, JumpHeight));
+				onground = false;
+				
+				animator.SetBool ("onground", onground);
+				//adds a force to jump
+				
 			} 
-
-	
-
-
-
+			
 		}
 	}
 	void FixedUpdate () {
 
-		bool  lookingup = Input.GetAxis ("Vertical")>0;
-		bool  islying = Input.GetAxis ("Vertical")<0;
 	
+	
+		float yaxis =Input.GetAxis ("Vertical");
+		animator.SetFloat ("yaxis",yaxis);
 
-		animator.SetBool ("islying",islying);
-		animator.SetBool ("lookingup",lookingup);
-		//jumpping 
-		//if( !onground)
-		{
+		//onliyingcolider.enabled = (yaxis < 0);
 
-			//Debug.Log ("FixedUpdate");
-			//jumpping
-			onground = Physics2D.OverlapCircle (groundCheck.position, radiousGround);
+		onground = Physics2D.OverlapCircle (groundCheck.position, radiousGround);
 
-			animator.SetBool ("onGround", onground);
+		animator.SetBool ("onground", onground);
+
+		//if(onground)
+		//animator.SetBool ("isjumping", false);
+
+		if(onground){
+			
+			if (Input.GetAxis ("Jump") > 0.1  ) {
+				animator.SetBool ("isjumping",true);
+				rigidBody2D.AddForce (new Vector2 (0, JumpHeight));
+				onground = false;
+				
+				animator.SetBool ("onground", onground);
+				//adds a force to jump
+				
+			} 
+			
 		}
 		//walking
 		float movx = Input.GetAxis("Horizontal");
@@ -78,12 +95,14 @@ public class PlayerController : MonoBehaviour {
 			Flip();
 		}
 
-		animator.SetFloat ("horSpeed",Mathf.Abs( movx));
+		animator.SetFloat ("xvel",Mathf.Abs( movx));
 
 		//if the player is jumpping the direction on X remains
 		Vector2 _vector = new Vector2 (movx * MaxHorizontalVelocity, rigidBody2D.velocity.y);
 		rigidBody2D.velocity = _vector;
 
+
+	
 	}
 
 	private void Flip(){

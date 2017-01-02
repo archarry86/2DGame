e29f5693dd709ff56a572d5f9526ea34ed738203
archarry86 <archarry86@gmail.com>
2017-01-02 01:19:15 -0500

    Red daemon animations, controller script and ,AI script

---
+++ 2DGame/Assets/Scripts/Rd_en_AI.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rd_en_AI : MonoBehaviour {
+
+	private UnityEngine.Random random = new UnityEngine.Random();
+
+
+	public enum RDStates{
+		ilde = 0,
+		walking = 1,
+		jumping = 2,
+		flying = 3,
+		dying = 4
+	}
+	
+	private float time ;
+	
+	public RDStates MyState = RDStates.ilde;
+
+	public Red_en_states controller;
+
+	public float Timetosetstate;
+	// Use this for initialization
+	void Start () {
+		time = Time.time + Timetosetstate;
+		controller = this.GetComponent<Red_en_states> ();
+
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		UnityEngine.Debug.Log("AI EN "+this.MyState);
+
+		if(Mathf.Abs( controller.riigbody.velocity.y) != 0)
+			MyState = RDStates.jumping;
+		else if( MyState == RDStates.jumping &&  controller.riigbody.velocity.y == 0)
+			MyState = RDStates.ilde;
+		
+	
+
+		var range = UnityEngine.Random.Range (0, 100);
+		if (time < Time.time) {
+			time= Time.time +Timetosetstate;
+
+			if (MyState == RDStates.ilde) {
+
+				if(range< 0.25f){
+					MyState = RDStates.walking;
+					controller.Direction = new Vector2(1,0);
+				}else	if(range> 0.25f){
+					MyState = RDStates.walking;
+					controller.Direction = new Vector2(-1,0);
+				}
+				else	if(range> 0.50f){
+					controller.Force = new Vector2(0,20);
+					MyState = RDStates.jumping;
+				}
+
+			}
+			else if(MyState == RDStates.walking){
+
+				//if(range< 0.10f){
+					//controller.Direction = new Vector2(0,0);
+					
+					//MyState = RDStates.ilde;
+					
+				//}else
+				{
+					UnityEngine.Debug.Log("walking "+controller.Direction);
+					controller.Direction = new Vector2(controller.Direction.x * -1, controller.Direction.y);
+
+				}
+
+
+
+			}
+			else if(MyState == RDStates.jumping){
+				if(controller.riigbody.velocity.y == 0)
+					MyState = RDStates.ilde;
+			}
+		}
+	}
+}
---
+++ 2DGame/Assets/Scripts/Red_en_states.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Red_en_states : MonoBehaviour {
+
+
+	public Animator animator;
+	public Transform transform;
+	public Rigidbody2D riigbody;
+
+	public bool facingRight;
+	public  float MaxHorizontalVelocity = 1.5f;
+
+	public EnemyHealth healthcontroller;
+
+	// Use this for initialization
+	void Start () {
+		animator = this.GetComponent<Animator> ();
+		transform = this.GetComponent<Transform> ();
+		Direction = new Vector2 (0, 0);
+		facingRight = false;
+		riigbody = this.GetComponent<Rigidbody2D> ();
+		healthcontroller = this.GetComponent<EnemyHealth> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void FixedUpdate () {
+
+		if (healthcontroller.IsAlive()) {
+
+			if (riigbody.velocity.y == 0 && Force.y != 0) {
+				riigbody.AddForce (new Vector2 (0, Force.y));
+			}
+			animator.SetFloat ("xaxis", Mathf.Abs (Direction.x));
+
+	
+
+			if (!facingRight && Direction.x > 0) {
+				Flip ();
+			} else 	if (facingRight && Direction.x < 0) {
+				Flip ();
+			}
+
+
+			animator.SetFloat ("yvel", riigbody.velocity.y);
+
+			Vector2 _vector = new Vector2 (Direction.x * MaxHorizontalVelocity, riigbody.velocity.y);
+			riigbody.velocity = _vector;
+
+			animator.SetBool ("onground", riigbody.velocity.y == 0);
+		}
+
+	}
+
+	private void Flip(){
+		facingRight = !facingRight;
+		Vector3 _localscale = transform.localScale;
+		_localscale.x *= -1;
+		transform.localScale = _localscale;
+	}
+
+
+	public Vector2 Direction{ 
+		get;
+		set;
+	}
+
+	public Vector2 Force{ 
+		get;
+		set;
+	}
+}
+
+
